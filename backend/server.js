1d74d5f407e69f81e8a127a083a164a7550ad688
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-15 11:31:19 -0500

    Connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const db = require('./db');
 
-const PORT = 8080;
+const PORT = 3001;
 const app = express();
 
 app.use(morgan('dev'));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server up on PORT ${PORT}`);
+
+  db.authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(err => console.error('Unable to connect to the database:', err));
+});
