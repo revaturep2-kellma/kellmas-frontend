ab60a9a65ca2dde8b64b2bc9ae9277737e943e25
jackson218 <wbjackson218@gmail.com>
2019-05-15 15:12:09 -0500

    added basic mySQL.sh and vm.sh scripts

---
+++ scripts/mySQL.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+az mysql server create --resource-group myresourcegroup --name mydemoserver  --location westus --admin-user myadmin --admin-password <server_admin_password> --sku-name GP_Gen5_2 --version 5.7
---
+++ scripts/vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VMname=$1
+groupName=$2
+
+az vm create 
+-n $VMname \
+-g $groupName \
+--size B1S \
+--generate-ssh-keys
