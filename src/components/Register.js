a97f9000c0c78797ae2409c11937e8e51f13e2f4
Matthew Schwartz <Mschw90@gmail.com>
2019-05-15 10:15:20 -0500

    added nav from reg to login and added label

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,47 +1,53 @@
 import React from 'react';
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            role: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      role: ''
 
-        };
-    }
-  
-  
-    render() {
-      return (
-        <div className="registerCon">
-            <h1>Kellma Cloud Create Account</h1>
-            <div>
-                <input 
-                    type="text" 
-                    value={this.state.name}
-                    onChange={(e) => this.setState({name: e.target.value})}
-                /><br/>
-                <input 
-                    type="text" 
-                    value={this.state.email}
-                    onChange={(e) => this.setState({email: e.target.value})}
-                /><br/>
-                <input 
-                    type="text" 
-                    value={this.state.password}
-                    onChange={(e) => this.setState({password: e.target.value})}
-                /><br/>
-                <input 
-                    type="text" 
-                    value={this.state.role}
-                    onChange={(e) => this.setState({role: e.target.value})}
-                />
-            </div>
-       </div>
-      );
-    }
+    };
   }
   
-  export default Register;
+  
+  render() {
+    return (
+      <div className="registerCon">
+        <h1>Kellma Cloud Create Account</h1>
+        <div>
+          <label for="Name">Name</label><br/>
+          <input 
+            type="text" 
+            value={this.state.name}
+            onChange={(e) => this.setState({name: e.target.value})}
+          /><br/>
+          <label for="Email">Email</label><br/>
+          <input 
+            type="text" 
+            value={this.state.email}
+            onChange={(e) => this.setState({email: e.target.value})}
+          /><br/>
+          <label for="Password">Password</label><br/>
+          <input 
+            type="text" 
+            value={this.state.password}
+            onChange={(e) => this.setState({password: e.target.value})}
+          /><br/>
+          <label for="Role">Role</label><br/>
+          <input 
+            type="text" 
+            value={this.state.role}
+            onChange={(e) => this.setState({role: e.target.value})}
+          />
+        </div>
+        <button onClick={() => this.props.history.push('/')}>Back to Login</button>
+
+      </div>
+    );
+  }
+}
+  
+export default Register;
