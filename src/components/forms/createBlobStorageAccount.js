f241e25feb155378a2568810f5f92d00c1809f6c
Matthew Schwartz <Mschw90@gmail.com>
2019-05-20 13:28:43 -0500

    blobstorage fetch made

--- src/components/forms/createBlobStorageAccount.js
+++ src/components/forms/createBlobStorageAccount.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { BASE_URL } from '../../config';
 
 class BlobStorageAccount extends React.Component {
   constructor(props) {
@@ -21,6 +22,41 @@ class BlobStorageAccount extends React.Component {
   handleCloseBlob = () => {
     this.setState({ openBlob: false });
   };
+
+  submit(groupName, blobName) {
+
+    const authToken = localStorage.getItem('authToken');
+
+    fetch(`${BASE_URL}/newUsers`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        groupName: groupName,
+        blobName: blobName
+      })
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+      /* do something with responseJson and go back to the Login view but
+       * make sure to check for responseJson.success! */
+        if(responseJson.success){
+          // return this.props.navigation.goBack();
+          this.handleCloseBlob();
+
+        }else{
+          alert(responseJson.error);
+          console.log('THERE WAS AN ERROR', responseJson.error);
+        }
+      })
+      .catch((err) => {
+        console.log('caught error in catch of submt');
+        alert(err);
+      /* do something if there was an error with fetching */
+      });
+  }
   
   
   render() {
@@ -45,7 +81,7 @@ class BlobStorageAccount extends React.Component {
               value={this.state.blobName}
               onChange={(e) => this.setState({blobName: e.target.value})}
             /><br/>
-            <button className="regBut">Create Blob</button>
+            <button className="regBut" onClick={ () => {this.submit(this.state.groupName, this.state.blobName);} }>Create Blob</button>
           </div>
         </Modal>
       </div>
